a4cebe560c2e5b9d108294bedec1410202a5ad76
spacey-pizzah <pizzah.spatial@gmail.com>
2023-02-24 16:04:55 +0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div style={{ background: 'transparent'}}>&#128515; &#128513;</div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
